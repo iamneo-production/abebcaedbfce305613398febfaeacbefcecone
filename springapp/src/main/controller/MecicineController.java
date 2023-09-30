c2a6ffbec97c9d55b21a594001aac42056ee6cf7
Iamneo <techadmin@iamneo.ai>
2023-09-30 06:42:02 +0000

    Backup Commit

---
+++ springapp/src/main/controller/MecicineController.java
@@ -0,0 +1,31 @@
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
+
+@RestController
+@RequestMapping("/medicines")
+public class MedicineController {
+    private Map<Integer, Medicine> medicineStore = new ConcurrentHashMap<>();
+    private AtomicInteger idGenerator = new AtomicInteger(1);
+
+    @PostMapping
+    public boolean addMedicine(@RequestBody Medicine medicine) {
+        int medicineId = idGenerator.getAndIncrement();
+        medicine.setMedicineId(medicineId);
+        medicineStore.put(medicineId, medicine);
+        return true;
+    }
+
+    @PutMapping("/{medicineId}")
+    public Medicine updateMedicine(@PathVariable int medicineId, @RequestBody Medicine updatedMedicine) {
+        if (medicineStore.containsKey(medicineId)) {
+            updatedMedicine.setMedicineId(medicineId);
+            medicineStore.put(medicineId, updatedMedicine);
+            return updatedMedicine;
+        }
+        return null; // Medicine not found
+    }
+}
